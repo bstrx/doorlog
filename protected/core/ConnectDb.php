b20831e84f6ec4d344cfa427abf370f57bb07a80
adrenaline <comstwork@gmail.com>
2013-02-05 13:53:43 +0400

    new commit

---
+++ protected/core/ConnectDb.php
@@ -0,0 +1,28 @@
+<?php
+
+class ConnectDb {
+    
+    protected static $_instance;
+    
+    private function __construct() {
+        
+    }
+ 
+    private function __clone() {
+        
+    }
+    
+    public static function getInstance() {
+        
+        if (null === self::$_instance) {
+            self::$_instance = new PDO ( 'mysql:host=localhost;dbname=information_schema', 'root', '123456');
+            self::$_instance->query ( 'SET character_set_connection = utf-8' );
+            self::$_instance->query ( 'SET character_set_client = utf-8' );
+            self::$_instance->query ( 'SET character_set_results = utf-8' );
+        }
+        
+        return self::$_instance;
+    }
+
+}
+?>
