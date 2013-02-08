cab9344dbc7fb3e7667c93655cc5be23158c1e62
Maslov <svyatoslav_maslov@mail.ru>
2013-02-08 11:15:11 +0400

    adding Autoloader

--- protected/core/ControllersManager.php
+++ protected/core/ControllersManager.php
@@ -1,31 +1,31 @@
 <?php
 
-         class ControllersManager{
-             private $test_url =  array();
-             
-             function getUrl(){
-                 $test_url = $_GET['route'];
-                 $url_arr=  explode("/", $test_url);
-                 $class = ucfirst($url_arr[0]);
-                 $method = $url_arr[1];
-                 if(class_exists($class, TRUE)){
-                     
-                     $obj = new $class;
-                     
-                 }
-                 
-                 if(method_exists($obj, $method)){
-                     
-                     $obj = $class->$method();
-                 }
-             }
-             
-             function __autoload($classname){
-                 require "controllers/".$classname.".php";
-             }
-             
-             
-             
-         }
+namespace core;
 
-?>
+class ControllersManager{
+    private $test_url =  array();
+
+    function getUrl(){
+        $test_url = $_GET['route'];
+        $url_arr=  explode("/", $test_url);
+        $class = ucfirst($url_arr[0]);
+        $method = $url_arr[1];
+        if(class_exists($class, TRUE)){
+
+            $obj = new $class;
+
+        }
+
+        if(method_exists($obj, $method)){
+
+            $obj = $class->$method();
+        }
+    }
+
+    function __autoload($classname){
+        require "controllers/".$classname.".php";
+    }
+
+
+
+}
