9a3ea95253ecdafb3d5fabdbade46c7193749051
Zeka <evgenia.gavriljuk@gmail.com>
2013-02-14 13:53:48 +0400

    Edit Autoloader.php index.php Controller.php

--- protected/core/Controller.php
+++ protected/core/Controller.php
@@ -1,11 +1,19 @@
 <?php
+
 namespace core;
 
-abstract class Controller
-{
-    protected function render(){
-        define('SMARTY_DIR', '/var/www/doorlog/protected/vendors/smarty/');
-        require_once (SMARTY_DIR . 'Smarty.class.php');
-        $smarty = new \Smarty();
+abstract class Controller {
+
+    protected function render($value) {
+        $smarty = new News\Smarty();
+
+        $smarty->setTemplateDir('/var/www/doorlog/protected/vendor/smarty/templates/');
+        $smarty->setCompileDir('/var/www/doorlog/protected/vendor/smarty/templates_c/');
+        $smarty->setConfigDir('/var/www/doorlog/protected/vendor/smarty/configs/');
+        $smarty->setCacheDir('/var/www/doorlog/protected/vendor/smarty/cache/');
+
+        $this->smarty->assign($value);
+        $smarty->display('protected/views/index.tpl');
     }
+
 }
