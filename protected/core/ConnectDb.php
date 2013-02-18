b534bcbe85f16ad52617bd0a340a282c19ff8dda
dupon21 <godrave@yandex.ru>
2013-02-18 13:08:48 +0400

    Update protected/core/ConnectDb.php

--- protected/core/ConnectDb.php
+++ protected/core/ConnectDb.php
@@ -17,7 +17,7 @@ class ConnectDb {
     public static function getInstance() {
         
        if (null === self::$_instance) {
-            self::$_instance = new \PDO( 'mysql:host=localhost;dbname=test', '', '');
+            self::$_instance = new \PDO( 'mysql:host=localhost;dbname=tc-db-main', 'root', '123456');
             self::$_instance->query ( 'SET character_set_connection = utf-8' );
             self::$_instance->query ( 'SET character_set_client = utf-8' );
             self::$_instance->query ( 'SET character_set_results = utf-8' );
