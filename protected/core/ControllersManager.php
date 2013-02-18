b94ca8d81151912de782287308480e493d54c13f
Zeka <evgenia.gavriljuk@gmail.com>
2013-02-18 12:21:08 +0400

    Edit ControllersManager.php

--- protected/core/ControllersManager.php
+++ protected/core/ControllersManager.php
@@ -2,45 +2,35 @@
 
 namespace core;
 
-class ControllersManager{
-    private $test_url =  array();
+class ControllersManager {
 
-    function getUrl(){
+    private $test_url = array();
+
+    function getUrl() {
 
         $test_url = $_GET['route'];
 
         if ($test_url) {
-            $url_arr=  explode("/", $test_url);
-            $class = "controllers\\".(ucfirst($url_arr[0]));
-            If(!$method = $url_arr[1]){
+            $url_arr = explode("/", $test_url);
+            $class = "controllers\\" . (ucfirst($url_arr[0]));
+            If (!$method = $url_arr[1]) {
                 $method = "index";
-            } else
-            {
+            } else {
                 $method = $url_arr[1];
             }
-
         } else {
             $class = "controllers\\Main";
             $method = "index";
         }
 
-//        if (!$test_url)
-//            {$url_arr=  explode("/", $test_url);
-//            $class = "controllers\\".(ucfirst($url_arr[0]));
-//            $method = "index";
-//
-//        }
-
-        if(class_exists($class, TRUE)){
+        if (class_exists($class, TRUE)) {
 
             $obj = new $class;
-
         }
 
-        if(method_exists($obj, $method)){
+        if (method_exists($obj, $method)) {
 
             $obj->$method();
-
         }
     }
 
