01766155d71186783e9319ac52241b733e19943c
Aleksandr Bugrov <aleksandr@amartirosyan.(none)>
2013-02-18 14:07:20 +0400

    Utils fixes

--- protected/core/Utils.php
+++ protected/core/Utils.php
@@ -1,15 +1,12 @@
 <?php
-
 namespace core;
 
-    class Utils{
-        
-        static function log($type, $data){
-           $db1 = ConnectDb::getInstance();           
-           $result1 = $db1->query("INSERT INTO log(type, data) 
-                 VALUES ('$type', '$data')");
-           return result1;
-        }     
-    }
-    
-    ?>
+class Utils{
+
+    static function log($type, $data){
+       $db = ConnectDb::getInstance();           
+       $result = $db->query("INSERT INTO log(type, data) 
+             VALUES ('$type', '$data')");
+       return result;
+    }     
+}
