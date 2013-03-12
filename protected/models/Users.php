9368d998be160cce1eec49464e57fec0382d2aea
Aleksandr Bugrov <aleksandr@amartirosyan.(none)>
2013-03-12 12:33:58 +0400

    fixes models/Users.php

--- protected/models/Users.php
+++ protected/models/Users.php
@@ -75,38 +75,7 @@ class Users extends Model{
         return $result[0];
     }
 
-    //Получает все периоды входов-выходов пользователя за определённый месяц
-    public function getMonthlyUserActions($userId, $timestamp = null) {
-        if (is_null($timestamp)) $timestamp = time();
-
-        $month = date('n', $timestamp);
-        $year = date('Y', $timestamp);
-
-        $q = "SELECT id,
-                logtime,
-                emphint,
-                DATE( logtime ) AS `day`,
-                TIME( logtime ) AS `time`,
-                SUBSTRING( HEX(  `logdata` ) , 10, 1 ) as direction
-            FROM `tc-db-log`.`logs`
-            WHERE
-              emphint = {$userId}
-              AND MONTH(logtime) = {$month}
-              AND YEAR(logtime) = {$year}
-            ORDER BY logtime ASC
-            ";
-
-        $result = $this->get($q);
-        return $result;
-    }
-
-    //Получает все периоды входов-выходов пользователя за определённую неделю
-    public function getWeeklyUserActions($userId, $timestamp = null) {
-        if (is_null($timestamp)) $timestamp = time();
-
-        $week = (int) date('W', $timestamp);
-        $year = date('Y', $timestamp);
-
+    public function getActions($userId, $fromDate, $toDate) {
         $q = "SELECT id,
                 logtime,
                 emphint,
