b7b700a4c462061cb596529f0f8e19ddf58b81a2
andrey <cherkasovas@inbox.ru>
2013-03-15 16:29:52 +0400

    depadd apart

--- protected/controllers/Departments.php
+++ protected/controllers/Departments.php
@@ -2,19 +2,30 @@
 namespace controllers;
 
 use core\Controller;
+use core\FlashMessages;
 use models\Departments as DepartmentModel;
 
 class Departments extends Controller {
 
     public function indexAction() {
         $obj =  new DepartmentModel();
+        $departments =  $obj->getAll();
+        $this->render("Departments/index.tpl" , array('departments' => $departments));
+    }
+
+    public function addAction() {
+        $obj =  new DepartmentModel();
         if(isset($_POST['depName']) && $_POST['depName']){
-           $depName = $_POST['depName'];
-           $obj->createDep($depName);
+            $depName = $_POST['depName'];
+            if ($obj->createDep($depName)){
+                FlashMessages::addMessage("Отдел успешно добавлен.", "info");
+            } 
+        } else {
+            FlashMessages::addMessage("Произошла ошибка. Отдел не был добавлен.", "error");
         }
 
         $departments =  $obj->getAll();
-        $this->render("Departments/index.tpl" , array('departments' => $departments));
+        $this->render("Departments/add.tpl" , array());
     }
 
     public function showAction(){
