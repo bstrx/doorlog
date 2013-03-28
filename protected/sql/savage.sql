de92be7d53357a04a7c16feb2737f3dfe93488f0
vladimir.prudilin <vladimir.prudilin@opensoftdev.ru>
2013-03-28 20:19:28 +0400

    .sql updated

--- protected/sql/savage.sql
+++ protected/sql/savage.sql
@@ -61,8 +61,8 @@ DROP TABLE IF EXISTS `permission`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `permission` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `key` char(20) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `name` char(20) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `key` varchar(100) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `name` varchar(100) character set utf8 collate utf8_unicode_ci NOT NULL,
   `permission_group_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `permission_group_id` (`permission_group_id`)
@@ -112,7 +112,7 @@ DROP TABLE IF EXISTS `position`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `position` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `name` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -123,7 +123,7 @@ CREATE TABLE `position` (
 
 LOCK TABLES `position` WRITE;
 /*!40000 ALTER TABLE `position` DISABLE KEYS */;
-INSERT INTO `position` VALUES (2,'Тестировщик'),(3,'Младший программист'),(4,'Старший программист');
+INSERT INTO `position` VALUES (2,'Тестировщик'),(3,'Младший программист'),(4,'Старший программист'),(6,'Должность 1');
 /*!40000 ALTER TABLE `position` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,7 +136,7 @@ DROP TABLE IF EXISTS `role`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
   `id` int(11) NOT NULL auto_increment,
-  `name` char(20) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `name` varchar(100) character set utf8 collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -147,7 +147,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'Админ'),(2,'Бухгалтер'),(4,'Новая роль');
+INSERT INTO `role` VALUES (1,'Админ'),(2,'Бухгалтер'),(6,'Роль');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,11 +159,11 @@ DROP TABLE IF EXISTS `roles_permissions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roles_permissions` (
-  `id_role` int(11) NOT NULL,
-  `id_permission` int(11) NOT NULL,
-  PRIMARY KEY  (`id_role`,`id_permission`),
-  KEY `id_role` (`id_role`),
-  KEY `id_permission` (`id_permission`)
+  `role_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY  (`role_id`,`permission_id`),
+  KEY `id_role` (`role_id`),
+  KEY `id_permission` (`permission_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -173,7 +173,7 @@ CREATE TABLE `roles_permissions` (
 
 LOCK TABLES `roles_permissions` WRITE;
 /*!40000 ALTER TABLE `roles_permissions` DISABLE KEYS */;
-INSERT INTO `roles_permissions` VALUES (1,1),(2,1),(2,2),(4,1);
+INSERT INTO `roles_permissions` VALUES (1,1),(1,2),(2,1),(2,2);
 /*!40000 ALTER TABLE `roles_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,6 +217,8 @@ CREATE TABLE `user` (
   `salt` varchar(100) NOT NULL,
   `department_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
+  `birthday` date NOT NULL,
+  `phone` varchar(20) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -227,7 +229,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (155,129,0,'godrave@yandex.ru','843a1bbbdf70c29e5995981a06bddd40aa852ab0','JT7m6',0,'2013-02-27 17:25:40'),(160,10,0,'groundsale@yandex.ru','703b058618d65461cc1b0b394710227999649458','DkYnk',0,'2013-02-28 18:50:14'),(161,120,0,'bstrxx@gmail.com','a64e68d6dcf5fd8320c86d657fbecf5128221681','N0pxn',0,'2013-03-01 11:33:29'),(162,64,0,'groundsale@yadd.ru','652ff0bdcd3465ef76123333faa47a2a743bc962','YfzOh',0,'2013-03-01 13:57:08'),(163,0,0,'','','',0,'2013-03-01 14:58:37'),(164,118,0,'electrik@toka.net','2149de1a8acd650e8bba1dd0c0f3ab0e61a9d33d','4bjH9',2,'2013-03-01 15:33:24'),(165,20,0,'elena','62a9a99fff7565968596dffa351dacf697a0353b','18xjk',1,'2013-03-01 16:14:24'),(166,59,0,'dm@dm','d2473920cb161f3838bfd933454ea8777f7c34b7','8Bhk9',0,'2013-03-01 17:29:55'),(169,95,0,'kd@kd.ru','4f85238e20d68d14fea6bf977e7f00bcd9635339','CEmNV',0,'2013-03-05 14:12:47'),(170,124,2,'kirill.kaplin@opensoftdev.ru','8306f51ea4aa7364b0bc832e5d3a165bcbe66037','RDxN8',4,'2013-03-07 12:34:29'),(171,123,2,'a@a','\nbe01c271f85d8f7d725720dba255db5c49c9bed2','T8VxH',4,'2013-03-07 12:37:35'),(172,122,0,'','7faaa029a3cfb1c080e2a2c89f69f6c616d2211c','YVb3M',1,'2013-03-12 20:43:22'),(173,96,0,'julia','acc3f4393900284d2771b0ad6cddfeabcc8fc9d0','tbebQ',5,'2013-03-13 12:54:38'),(174,98,0,'fsfsd','38b83d5bf752212576f8352f4bcdb3f3bc63d685','5dweI',4,'2013-03-14 15:03:03'),(175,130,0,'groundsale@yandex.ru','5be25f136e35ac5a46dbb2245fe387ed665f8348','HykUF',1,'2013-03-14 17:42:37'),(176,21,0,'groundsale@yandex.ru','da3088aeaf4d888ca7179fdc43aa89d9968cc868','B86In',4,'2013-03-14 17:56:09'),(177,24,0,'bstrxx@gmail.com','46cf360f199b0d4ff4f2dffc1effbfa2016e1489','XyN9o',2,'2013-03-14 17:59:56'),(178,45,0,'groundsale@yandex.ru','f5f952401db3906153b4e048dc2714c30f8b59fb','sp4kz',2,'2013-03-14 18:01:56'),(179,26,0,'bstrxx@gmail.com','55fc6ce62df104dc7e75ca931db4f8d0a5be752e','kDGIN',5,'2013-03-14 18:02:46'),(180,51,0,'t','2d9d1aef9d6eaec29d7a84bf093b60c270e93e55','UwEBx',2,'2013-03-18 15:51:58');
+INSERT INTO `user` VALUES (155,129,0,'godrave@yandex.ru','843a1bbbdf70c29e5995981a06bddd40aa852ab0','JT7m6',0,'2013-02-27 17:25:40','0000-00-00',''),(160,10,0,'groundsale@yandex.ru','703b058618d65461cc1b0b394710227999649458','DkYnk',0,'2013-02-28 18:50:14','0000-00-00',''),(161,120,0,'bstrxx@gmail.com','a64e68d6dcf5fd8320c86d657fbecf5128221681','N0pxn',0,'2013-03-01 11:33:29','0000-00-00',''),(162,64,0,'groundsale@yadd.ru','652ff0bdcd3465ef76123333faa47a2a743bc962','YfzOh',0,'2013-03-01 13:57:08','0000-00-00',''),(163,0,0,'','','',0,'2013-03-01 14:58:37','0000-00-00',''),(164,118,0,'electrik@toka.net','2149de1a8acd650e8bba1dd0c0f3ab0e61a9d33d','4bjH9',2,'2013-03-01 15:33:24','0000-00-00',''),(165,20,0,'elena','62a9a99fff7565968596dffa351dacf697a0353b','18xjk',1,'2013-03-01 16:14:24','0000-00-00',''),(166,59,0,'dm@dm','d2473920cb161f3838bfd933454ea8777f7c34b7','8Bhk9',0,'2013-03-01 17:29:55','0000-00-00',''),(169,95,0,'kd@kd.ru','4f85238e20d68d14fea6bf977e7f00bcd9635339','CEmNV',0,'2013-03-05 14:12:47','0000-00-00',''),(170,124,2,'kirill.kaplin@opensoftdev.ru','8306f51ea4aa7364b0bc832e5d3a165bcbe66037','RDxN8',4,'2013-03-07 12:34:29','0000-00-00',''),(171,123,2,'a@a','\nbe01c271f85d8f7d725720dba255db5c49c9bed2','T8VxH',4,'2013-03-07 12:37:35','0000-00-00',''),(172,122,0,'','7faaa029a3cfb1c080e2a2c89f69f6c616d2211c','YVb3M',1,'2013-03-12 20:43:22','0000-00-00',''),(173,96,0,'julia','acc3f4393900284d2771b0ad6cddfeabcc8fc9d0','tbebQ',5,'2013-03-13 12:54:38','0000-00-00',''),(174,98,0,'fsfsd','38b83d5bf752212576f8352f4bcdb3f3bc63d685','5dweI',4,'2013-03-14 15:03:03','0000-00-00',''),(175,130,0,'groundsale@yandex.ru','5be25f136e35ac5a46dbb2245fe387ed665f8348','HykUF',1,'2013-03-14 17:42:37','0000-00-00',''),(176,21,0,'groundsale@yandex.ru','da3088aeaf4d888ca7179fdc43aa89d9968cc868','B86In',4,'2013-03-14 17:56:09','0000-00-00',''),(177,24,0,'bstrxx@gmail.com','46cf360f199b0d4ff4f2dffc1effbfa2016e1489','XyN9o',2,'2013-03-14 17:59:56','0000-00-00',''),(178,45,0,'groundsale@yandex.ru','f5f952401db3906153b4e048dc2714c30f8b59fb','sp4kz',2,'2013-03-14 18:01:56','0000-00-00',''),(179,26,0,'bstrxx@gmail.com','55fc6ce62df104dc7e75ca931db4f8d0a5be752e','kDGIN',5,'2013-03-14 18:02:46','0000-00-00',''),(180,51,0,'t','2d9d1aef9d6eaec29d7a84bf093b60c270e93e55','UwEBx',2,'2013-03-18 15:51:58','0000-00-00',''),(181,125,4,'dadwa@daw.ru','39889bce4a3a3e6a308ecb0a53454241669358e5','LpaWi',2,'2013-03-28 19:52:07','2013-03-20','123123789');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -239,11 +241,11 @@ DROP TABLE IF EXISTS `users_roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users_roles` (
-  `id_person` int(11) unsigned NOT NULL,
-  `id_role` int(11) NOT NULL,
-  PRIMARY KEY  (`id_role`,`id_person`),
-  KEY `id_person` (`id_person`),
-  KEY `id_role` (`id_role`)
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY  (`role_id`,`user_id`),
+  KEY `id_person` (`user_id`),
+  KEY `id_role` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -291,4 +293,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-03-28 18:02:45
+-- Dump completed on 2013-03-28 20:16:18
