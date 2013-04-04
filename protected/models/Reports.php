20c7f017c8468dfaa4ad296ef392e205201c629e
vladimir.prudilin <vladimir.prudilin@opensoftdev.ru>
2013-04-04 17:57:22 +0400

    Sql fix

--- protected/models/Reports.php
+++ protected/models/Reports.php
@@ -15,7 +15,7 @@ class Reports extends Model{
         $params['date'] = $date;
         $q = "SELECT count(emphint) AS count, HOUR(logtime) as hour
               FROM `tc-db-log`.`logs`
-              WHERE DATE(logtime) = DATE(:date) AND SUBSTRING( HEX ( `logdata` ) , 10, 1 ) = 2
+              WHERE DATE(logtime) = DATE(:date) AND SUBSTRING( HEX( `logdata` ) , 10, 1 ) = 2
               GROUP BY HOUR(logtime)
               ORDER BY HOUR(logtime) ASC";
         $result = $this->fetchAll($q, $params);
