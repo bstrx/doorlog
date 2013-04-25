458f57680cc9fcc97bc5c43acb297bf24cd81dbf
dmitry konurov <dmtiry.konurov@opensoftdev.ru>
2013-04-25 12:07:35 +0400

    Autocomplete User

---
+++ assets/js/AutocompleteUser.js
@@ -0,0 +1,44 @@
+    $(function() {
+        $("#autocomplete").autocomplete({
+            minLength: 3,
+            delay: 600,
+            source: function( request, response ) {
+                $.ajax({
+                    url: "/users/autocomplete",
+                    dataType: "json",
+                    data:{
+                        name:request.term
+                    },
+
+                    success: function(data) {
+                        response($.map(data, function(item) {
+                            if (typeof(item.dep) == 'string')  item.dep = " | " + item.dep;
+                                else item.dep = '';
+                            if (typeof(item.pos) == 'string')  item.pos = " | " + item.pos;
+                                else item.pos = '';
+                            return {
+                                label: item.name + item.dep + item.pos,
+                                id:item.id
+                            };
+                        }));
+                    }
+                });
+            },
+            select: function( event, ui ) {
+                $("#id").val(ui.item.id);
+            },
+            messages: {
+                noResults: '',
+                results: function() {
+                }
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $('#autocomplete').keyup(function (e) {
+            if((e.keyCode!=37) && (e.keyCode!=38) && (e.keyCode!=39) && (e.keyCode!=40) && (e.keyCode!=13)){
+                $("#id").val('');
+            }
+        });
+    });
